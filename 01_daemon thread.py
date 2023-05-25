cae7a4142adffe681ab15c2c51e95c8205c1e2f1
Pritirekha23 <learnpriti23@gmail.com>
2023-05-26 01:13:18 +0530

    day 83 of 100days of code

--- 01_daemon thread.py
+++ 01_daemon thread.py
@@ -175,3 +175,57 @@ Hi
 Hi
 Hi
 End of the main thread '''
+
+#ex-9
+'''def fun():
+    for i in range(5):
+        print('Hi')
+        time.sleep(1)
+    t2=Thread(target=fun1,daemon=False)
+    print('t2 is daemon:',t2.daemon)
+    t2.start()
+def fun1():
+    for i in range(5):
+        print('Hello')
+        time.sleep(1)
+
+t1=Thread(target=fun,daemon=True)
+print('t1 is daemon:',t1.daemon)
+t1.start()
+time.sleep(7)
+print('End of the main thread')
+
+t1 is daemon: True
+Hi
+Hi
+Hi
+Hi
+Hi
+t2 is daemon: False
+Hello
+Hello
+End of the main thread
+Hello
+Hello
+Hello'''
+
+#ex-10
+'''
+def sec_count():
+    sec=0
+    while True:
+        time.sleep(4)
+        sec=sec+4
+        print(f'I am waiting since {sec} second')
+
+t1=Thread(target=sec_count,daemon=True)
+t1.start()
+name=input('Enter your name:\n')
+
+Enter your name:
+I am waiting since 4 second
+I am waiting since 8 second
+I am waiting since 12 second
+I am waiting since 16 second
+priti
+'''
