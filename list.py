61437bceec15e86510356a8e3cf4b54a0842b531
Pritirekha23 <learnpriti23@gmail.com>
2023-06-10 21:03:02 +0530

    new pr

---
+++ list.py
@@ -0,0 +1,39 @@
+# list=[23,33,'surendrs',9.9]
+# print(list)
+
+# list=[]
+# print(list)
+
+# list=eval((input("Enter a list:")))
+# print(list)
+# Enter a list:[2,3,4,6]
+# [2, 3, 4, 6]
+# list=list((2,3,'surendra'))
+# print(list)
+
+
+# list=list(range(5))
+# print(list)
+
+# list=[23,44,5.3,'surendra']
+# print(list)
+
+# whenever it will find a space split it
+# message='Hel-lo-this-is-P-y-thon'
+# print(message.split('-'))
+#list=message.split()
+# print(list)
+
+
+# list=[23,33,'surendra',9.9]
+# print(list[3])
+# print(list[-3])
+# print(list[0])
+# 9.9
+# 33
+# 23
+list=[23,33,'surendra',9.9]
+print(list[::])
+print(list[1::2])
+[23, 33, 'surendra', 9.9]
+[33, 9.9]
