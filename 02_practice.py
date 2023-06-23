95d958e1b803678c338a6c6caf7c1d5aa5948fba
Pritirekha23 <learnpriti23@gmail.com>
2023-06-23 08:03:52 +0530

    adding a number is prime or not

---
+++ 02_practice.py
@@ -0,0 +1,25 @@
+# a=10+2j
+# print(a.real)
+# print(a.imag)
+
+# st='''
+#     Hi 
+#       this  
+#           is
+#              priti'''
+# print(st) 
+     
+# st2='This is slice operator'
+# print(st2[0:9:2])
+# print(st2[::2])
+# print(len(st2))
+# print(st2[::-1])
+# print(st2[0::])
+
+st3='welcome to python'
+# print(st3[8:15:-1])
+# print(st3[-5:-18:-1])
+# print(st3[-5:-10:-4])
+# print(st3[-1:-4:-1])
+# print(st3[:-4:])
+print(st3[True:-1:1])
---
+++ Questions/01_prime-number.py
@@ -0,0 +1,78 @@
+# def prime(num):
+#     if num<=1:
+#         return False
+  
+#     for i in range(2,int(num**0.5)+1):
+#         if num%i==0:
+#             return False
+#     return True
+
+# num=int(input("Enter a number::"))
+# if prime(num):
+#     print(f'{num} is a prime number')
+# else:
+#     print(f'{num} is not a prime numebr'
+# num=int(input("Enter a number::"))
+
+# 
+  
+# if num>1:
+#     for i in range(2,int(num/2)+1):
+#         if num%i==0:
+#             print(f'{num} is not a prime number')
+#             break
+        
+        
+#     else:
+#          print(f'{num} is a prime number')
+# else:
+#     print(f'{num} is not a prime number')
+
+
+
+# m3
+
+# number=int(input("ENter a umber:"))
+
+# for p in range(2,number):
+#     if number%p==0:
+#         print("not a prime number")
+#         break
+# else:
+#     print('prime number')
+
+
+
+
+'''m4'''
+
+
+# To take input from the user
+# num = int(input("Enter a number: "))
+
+# if num == 1:
+#     print(num, "is not a prime number")
+# elif num > 1:
+#    # check for factors
+#    for i in range(2,num):
+#        if (num % i) == 0:
+#            print(num,"is not a prime number")
+#           # print(i,"times",num//i,"is",num)
+#            break
+#    else:
+#        print(num,"is a prime number")
+       
+# # if input number is less than
+# # or equal to 1, it is not prime
+# else:
+#    print(num,"is not a prime number")
+
+
+
+num=int(input("Enter a number::"))
+for i in range(2,num):
+    if num%i==0:
+        print('not prime')
+        break
+else:
+    print('prime')
